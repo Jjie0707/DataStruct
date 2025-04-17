7164a3d8314f141e6e80555626d21fc28ffeb18b
Jjie0707 <1778836683@qq.com>
2025-04-17 10:15:38 +0800

    04/17(1)

---
+++ Jjie_4_17/src/test0/Test.java
@@ -0,0 +1,56 @@
+package test0;
+
+public class Test {
+    public static void main(String[] args) {
+        int[] arr = {3, 1, 4, 3, 2, 2, 4};
+        int k = 2;
+        System.out.println(Solution.countGood(arr, k));
+    }
+
+    class Solution {
+        public static boolean check(int[] arr, int start, int end, int k) {
+            int count = 0;
+            for (int i = start; i <= end; i++) {
+                for (int j = i + 1; j <= end; j++) {
+                    if (arr[i] == arr[j]) {
+                        count++;
+                    }
+                    if (count >= k) {
+                        return true;
+                    }
+                }
+            }
+            return count == k;
+        }
+
+        public static long countGood(int[] nums, int k) {
+            long res = 0;
+
+            int length = 2;
+            while (true) {
+                if ((length * (length - 1) >= 2 * k)) {
+                    break;
+                }
+                length++;
+            }
+            int left = 0;
+            int right = left + length - 1;
+
+            int end = nums.length - 1;
+            if (right > end) {
+                return res;
+            }
+            //不断移动右边界直到成为好数组
+            for(int i=0;i+length-1<=end;i++){
+                //length是要达到k好数组的最小长度
+                left=i;
+                right= left + length - 1;
+                while (right+1<=end && !check(nums, left, right, k)) {
+                    right++;
+                }
+                res+=(end-right+1);
+            }
+            return res;
+        }
+    }
+}
