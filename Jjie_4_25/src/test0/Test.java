c99c0a2c77d7a25166ad7929098d36be89567d70
Jjie0707 <1778836683@qq.com>
2025-04-25 23:54:25 +0800

    04/25(2)

--- Jjie_4_25/src/test0/Test.java
+++ Jjie_4_25/src/test0/Test.java
@@ -1,8 +1,59 @@
 package test0;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Test {
     public static void main(String[] args) {
-        MinStack s=new MinStack();
+
+        ArrayList<Integer> arr=new ArrayList<>();
+        arr.add(3);
+        arr.add(2);
+        arr.add(4);
+
+        int m=2;
+        int k=1;
+        System.out.println(countInterestingSubarrays(arr,m,k));
+    }
+    public static long countInterestingSubarrays(List<Integer> nums, int m, int k) {
+        int length= nums.size();
+
+        int[] arr=new int[length];
+        for(int i=0;i<length;i++){
+            arr[i]=nums.get(i) % m;
+        }
+
+        int count=0;
+        int[] pre=new int[length];
+        for(int i=0;i<length;i++){
+            if(arr[i]==k){
+                count++;
+            }
+            pre[i]=count;
+        }
+        //完成了前缀和的统计
+
+        int ret=0;
+        for(int i=0;i<length;i++){
+            if(k==0 && arr[i]!=k){
+                ret++;
+                continue;
+            } else if (k==1 && arr[i]==k) {
+                ret++;
+                continue;
+            }
+            if(pre[i]%m==k){
+                ret++;
+            }
+        }
+        return ret;
+    }
+    public static void main1(String[] args) {
+        System.out.println("hello world");
+    }
+
+    public static void main0(String[] args) {
+        MinStack s = new MinStack();
         s.push(1);
         s.push(3);
         s.push(9);
@@ -16,5 +67,34 @@ public class Test {
         s.pop();
         s.push(9999);
         System.out.println(s.top());
+//        List
     }
 }
+/*
+class Solution {
+    public long countInterestingSubarrays(List<Integer> nums, int m, int k) {
+        int length= nums.size();
+
+        int[] arr=new int[length];
+        for(int i=0;i<length;i++){
+            arr[i]=nums.get(i) % m;
+        }
+
+        int count=0;
+        int[] pre=new int[length];
+        for(int i=0;i<length;i++){
+            if(arr[i]==k){
+                count++;
+            }
+            pre[i]=count;
+        }
+        //完成了前缀和的统计
+
+        int ret=0;
+        for(int i=0;i<length;i++){
+
+        }
+        return ret;
+    }
+}
+ */
