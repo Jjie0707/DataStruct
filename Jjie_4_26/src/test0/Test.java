beb5127de3e356ea350b673b5de85e4be0a78eef
Jjie0707 <1778836683@qq.com>
2025-04-26 22:54:39 +0800

    04/26(1)

---
+++ Jjie_4_26/src/test0/Test.java
@@ -0,0 +1,17 @@
+package test0;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Test {
+   LinkedList
+//    public void push(int val){
+//        if(!qu1.isEmpty()){
+//            qu1.offer(val);
+//        }else if(!qu2.isEmpty){
+//            qu2.offer(val);
+//        }else{
+//            qu1.offer(val);
+//        }
+//    }
+}
