c37385f517d911cf52e1ab5b58a02e82281c33c1
Jjie0707 <1778836683@qq.com>
2025-05-25 23:39:29 +0800

    05/25(2)

--- Jjie_5_25/src/test2/BinarySearchTree.java
+++ Jjie_5_25/src/test2/BinarySearchTree.java
@@ -85,14 +85,29 @@ public class BinarySearchTree {
                         curp.left=cur.left;
                     }
                 }else{
-                    //
+                    //走到这里就是两边都不为空
+                    // 替换删除 找到右边的最小值来替换
+                    //右边最小值的左边一定为空
+                    TreeNode tem=cur.right;
+                    TreeNode temp=cur;
+                    while(tem.left!=null){
+                        temp=tem;
+                        tem = tem.left;
+                    }
+                    //走到这里说明找到了右边的最小值
+                    cur.key = tem.key;
+                    if (cur.right == tem) {
+                        cur.right = tem.right;
+                    } else {
+                        temp.left = tem.right;
+                    }
                 }
-            }else if(cur.key>key){
-                curp=cur;
-                cur=cur.left;
-            }else{
-                curp=cur;
-                cur=cur.right;
+            } else if (cur.key > key) {
+                curp = cur;
+                cur = cur.left;
+            } else {
+                curp = cur;
+                cur = cur.right;
             }
         }
         return false;
