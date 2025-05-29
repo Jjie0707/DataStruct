94f5b922eadda0e017858f6a3b05b932368ab1ba
Jjie0707 <1778836683@qq.com>
2025-05-29 15:30:31 +0800

    05/29（1）

--- Game/src/game/Game.java
+++ Game/src/game/Game.java
@@ -10,23 +10,50 @@ public class Game extends JFrame {
         initGame();
 
         //接下来完成主游戏界面的菜单设置
+        initMenu();
+
+
+
+
+
+        //默认隐藏，改成可见
+        this.setVisible(true);
+    }
+
+
+
+
+
+
+
+
+    private void initMenu() {
         JMenuBar menubar=new JMenuBar();
 
         JMenu functionmenu=new JMenu("功能");
-        
-
+        JMenuItem change=new JMenuItem("更换图片");
+        JMenuItem relog=new JMenuItem("重新登录");
+        JMenuItem regame=new JMenuItem("重新游戏");
+        JMenuItem exit=new JMenuItem("退出游戏");
+        functionmenu.add(change);
+        functionmenu.add(regame);
+        functionmenu.add(relog);
+        functionmenu.add(exit);
 
 
         JMenu aboutmenu=new JMenu("关于我");
+        JMenuItem wechat=new JMenuItem("微信");
+        aboutmenu.add(wechat);
 
+        menubar.add(functionmenu);
+        menubar.add(aboutmenu);
 
+        this.setJMenuBar(menubar);
     }
 
     private void initGame() {
         this.setSize(600,600);
 
-        //默认隐藏，改成可见
-        this.setVisible(true);
         //设置界面名字
         this.setTitle("拼图游戏（单机）");
         //设置关闭模式，关闭一个页面全部关闭
